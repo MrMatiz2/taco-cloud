fbf11b62405f12ead7bb1a1eef5d3d147f819c37
MrMatiz2 <alejandromatiz87@gmail.com>
2024-11-11 16:32:17 -0500

    HomeControllerTest error resolved

--- src/test/java/com/example/tacocloud/HomeControllerTest.java
+++ src/test/java/com/example/tacocloud/HomeControllerTest.java
@@ -2,26 +2,27 @@ package com.example.tacocloud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HomeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    //TODO Validate Test failing
-    /* @Test
+    @Test
     public void testHomePage() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"))
                 .andExpect(content().string(containsString("Welcome to...")));
-    } */
+    }
 
 }
