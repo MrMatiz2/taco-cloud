512c70da524b4766ce10cc0d5871d33da91625dc
MrMatiz2 <alejandromatiz87@gmail.com>
2024-11-23 23:13:07 -0500

    APIs with CSRF

--- src/main/java/com/example/tacocloud/configuration/SecurityConfig.java
+++ src/main/java/com/example/tacocloud/configuration/SecurityConfig.java
@@ -41,9 +41,8 @@ public class SecurityConfig {
                 .formLogin(form -> form
                         .loginPage("/login")
                         .defaultSuccessUrl("/design", true)
-                ).csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/api/*")
-                ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
 
