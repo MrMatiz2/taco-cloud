c1cfcf354e5a3e63ccecc6b7c2dff8b6b0c839fa
MrMatiz2 <alejandromatiz87@gmail.com>
2025-01-05 17:07:37 -0500

    Revert WebFlux implementation

--- src/main/java/com/example/tacocloud/controllers/TacoController.java
+++ src/main/java/com/example/tacocloud/controllers/TacoController.java
@@ -2,10 +2,12 @@ package com.example.tacocloud.controllers;
 
 import com.example.tacocloud.repositories.TacoRepository;
 import com.example.tacoclouddomain.entities.Taco;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/tacos", produces = "application/json")
@@ -19,19 +21,24 @@ public class TacoController {
     }
 
     @GetMapping(params = "recent")
-    public Flux<Taco> recentTacos() {
-        return tacoRepository.findAll().take(12);
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        return tacoRepository.findAll(page).getContent();
     }
 
     @GetMapping("{id}")
-    public Mono<Taco> tacoById(@PathVariable long id) {
-        return tacoRepository.findById(id);
+    public ResponseEntity<?> tacoById(@PathVariable long id) {
+        Optional<Taco> taco = tacoRepository.findById(id);
+        if (taco.isPresent()) {
+            return ResponseEntity.ok(taco.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping(consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
-    public Mono<Taco> saveTaco(@RequestBody Mono<Taco> taco) {
-        return tacoRepository.saveAll(taco).next();
+    public Taco saveTaco(@RequestBody Taco taco) {
+        return tacoRepository.save(taco);
     }
 
 }
--- src/main/java/com/example/tacocloud/repositories/TacoRepository.java
+++ src/main/java/com/example/tacocloud/repositories/TacoRepository.java
@@ -1,8 +1,9 @@
 package com.example.tacocloud.repositories;
 
 import com.example.tacoclouddomain.entities.Taco;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public interface TacoRepository extends PagingAndSortingRepository<Taco, Long>, ReactiveCrudRepository<Taco, Long> {
+public interface TacoRepository extends PagingAndSortingRepository<Taco, Long>, CrudRepository<Taco, Long> {
 }
